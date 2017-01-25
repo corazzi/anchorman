a8667a1476ecb5cdf237e312d587af6b230eb161
Kieran Marshall <kieran@kieranmarshall.co.uk>
2017-01-25 22:20:25 +0000

    Initial commit.

---
+++ src/Presenter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Kayrunm\Anchorman;
+
+abstract class Presenter
+{
+    /**
+     * The entity that this class will act as a presenter for
+     * @var [type]
+     */
+    protected $entity;
+
+    /**
+     * Set up the presenter class by storing the associated model.
+     *
+     * @param \Illuminate\Eloquent\Model  $entity
+     */
+    public function __construct($entity)
+    {
+        $this->entity = $entity;
+    }
+
+    /**
+     * Our attribute getter tries to return a method on this class with the
+     * equivalent name to the camelCase version of the property accessed,
+     * or it will try return the same property from the parent entity.
+     *
+     * @param  string  $property  The presenter to return
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        // Get a camel-case version of the property called
+        $key = Str::snake($property);
+
+        if (method_exists($this, $property)) {
+            return $this->{$property}();
+        }
+
+        return $this->entity->getAttribute($property);
+    }
+}
---
+++ src/Traits/Presentable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Kayrunm\Anchorman\Traits;
+
+/**
+ * This might be slightly pointless, but it's useful to indicate to someone that
+ * the model that is using this trait is using a presenter implementation.
+ */
+trait Presentable
+{
+    /**
+     * The presenter for this view.
+     *
+     * @var string
+     */
+    protected $presenter;
+
+    /**
+     * The instance of our presenter
+     *
+     * @var \Kayrunm\Anchorman\Presenter
+     */
+    protected $presenterInstance;
+
+    /**
+     * Return an instance of our presenter instead of the fully-qualified
+     * class name.
+     *
+     * @return \Kayrunm\Anchorman\Presenter
+     */
+    public function getPresenterAttribute()
+    {
+        if (is_null($this->presenterInstance)) {
+            $this->presenterInstance = new $presenter($this);
+        }
+
+        return $this->presenterInstance;
+    }
+}
